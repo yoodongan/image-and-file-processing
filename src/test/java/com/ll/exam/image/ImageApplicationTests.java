aef0221e22d62d8889e6e7ecaa3fc14e0a80e6a4
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-09-05 15:12:34 +0900

    추가 세팅

--- src/test/java/com/ll/exam/image/ImageApplicationTests.java
+++ src/test/java/com/ll/exam/image/ImageApplicationTests.java
@@ -1,9 +1,12 @@
 package com.ll.exam.image;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
+@EnableJpaAuditing
 class ImageApplicationTests {
 
 	@Test
