b9ddd11855f780b51813f90e88372af2ab197de2
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-09-06 14:14:28 +0900

    파일 2개 업로드 하여 저장, csrf 체크 끄기, 업로드한 파일 웹서버에서 접근 가능하도록

--- src/main/java/com/ll/exam/image/app/home/HomeController.java
+++ src/main/java/com/ll/exam/image/app/home/HomeController.java
@@ -2,7 +2,12 @@ package com.ll.exam.image.app.home;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 @Controller
 @RequiredArgsConstructor
@@ -10,5 +15,22 @@ public class HomeController {
     @RequestMapping("/")
     public String main() {
         return "home/main";
+    }
+
+    @RequestMapping("/test/upload")
+    public String upload() {
+        return "home/test/upload";
+    }
+
+    @PostMapping("/test/upload")
+    public void upload2(MultipartFile[] uploadFile, Model model) {
+        String uploadFolder = "c:/temp/app10/1.png";
+        for (MultipartFile multipartFile : uploadFile) {
+            File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+            try {
+                multipartFile.transferTo(saveFile);
+            } catch (Exception e) {
+            }
+        }
     }
 }
